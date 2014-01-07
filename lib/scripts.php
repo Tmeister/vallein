d79b3136ff55c0363bd836e8189d8123f63d9106
Enrique Chavez <noone@tmeister.net>
2014-01-07 14:55:08 -0600

    Site width and tour module
    Signed-off-by: Enrique Chavez <noone@tmeister.net>

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/base.min.css', false, 'a98e41e29dcef85af97e782677197129');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/base.min.css', false, 'a9262f9f7e2734cb204c0755683b6153');
   wp_enqueue_style('theme_main', get_template_directory_uri() . '/assets/less/theme-custom.less');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
